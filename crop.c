e79d79c28e0feb06de8833434295b9bb59eddced
DJasmineJoyline <42811221+DJasmineJoyline@users.noreply.github.com>
2019-10-12 17:09:32 +0530

    Add files via upload

---
+++ crop.c
@@ -0,0 +1,29 @@
+
+ 
+
+
+
+
+#include<stdio.h>
+int main()
+{
+  int num,m,count=0;
+  scanf("%d",&num);
+  while(num>=30)
+  {
+      num=num-30;
+      count++;
+      scanf("%d",&m);
+      while(m>0)
+      {
+          num=num*2;
+          m--;
+      }
+      
+      
+  }
+  printf("%d",count);
+	
+	
+	return 0;
+}
